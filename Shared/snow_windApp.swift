512393482c664b1d401ebcdb3ae1d5e8bdb7fb69
ruanchenhao <30828481+ladyishenlong@users.noreply.github.com>
2021-12-17 14:37:17 +0800

    Initial Commit

---
+++ Shared/snow_windApp.swift
@@ -0,0 +1,17 @@
+//
+//  snow_windApp.swift
+//  Shared
+//
+//  Created by 阮陈豪 on 2021/12/17.
+//
+
+import SwiftUI
+
+@main
+struct snow_windApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
